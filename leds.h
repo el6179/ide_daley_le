4597ed39b0567079bc6403fbacd49dd1d42058e4
el6179 <60402612+el6179@users.noreply.github.com>
2022-02-16 15:02:37 -0500

    Create leds.h

---
+++ leds.h
@@ -0,0 +1,18 @@
+#ifndef _LEDSH_
+#define _LEDSH_
+
+typedef unsigned char BOOLEAN;
+#define FALSE 0
+#define TRUE  !FALSE
+typedef unsigned char BYTE;
+
+void LED1_Init(void);
+void LED2_Init(void);
+void LED1_On(void);
+void LED1_Off(void);
+
+BOOLEAN LED1_State(void);
+
+void LED_ON(int);
+
+#endif
