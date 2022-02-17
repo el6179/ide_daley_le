af36a2bc49bbacb9bd7cffe44b24e93eaa3e5706
el6179 <60402612+el6179@users.noreply.github.com>
2022-02-17 16:19:42 -0500

    Create switches.h

---
+++ switches.h
@@ -0,0 +1,12 @@
+#ifndef _SWITCHESH_
+#define _SWITCHESH_
+
+typedef unsigned char BOOLEAN;
+#define FALSE 0
+#define TRUE  !FALSE
+typedef unsigned char BYTE;
+
+void Switch1_Init(void);
+void Switch2_Init(void);
+
+#endif
