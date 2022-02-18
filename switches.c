e58c8124583a65382751eb7dc1a3a72b63675d4c
ted5363 <47145977+ted5363@users.noreply.github.com>
2022-02-18 15:23:11 -0500

    Update switches.c

--- switches.c
+++ switches.c
@@ -14,6 +14,7 @@ void Switch1_Init(void){
 	   
 	// configure as input
      P1->DIR &= ~BIT1;
+	   P1->OUT |= BIT1;
 	   P1->REN |= BIT1;
               
 }
@@ -24,5 +25,37 @@ void Switch2_Init(void){
 	   P1->SEL1 &= ~BIT4;
 	// configure as input
      P1->DIR &= ~BIT4;
+	   P1->OUT |= BIT4;
 	   P1->REN |= BIT4;	
+}
+
+//------------Switch_Input------------
+// Read and return the status of Switch1
+// Input: none
+// return: TRUE if pressed
+//         FALSE if not pressed
+BOOLEAN Switch1_Pressed(void)
+{
+	BOOLEAN retVal = FALSE;
+	// check if pressed
+	if (P1->OUT & BIT1){
+		retVal =  TRUE;
+	}
+
+	return (retVal);              // return TRUE(pressed) or FALSE(not pressed)
+}
+//------------Switch_Input------------
+// Read and return the status of Switch2
+// Input: none
+// return: TRUE if pressed
+//         FALSE if not pressed
+BOOLEAN Switch2_Pressed(void)
+{
+	BOOLEAN retVal = FALSE;
+	// check if pressed
+	if (P1->OUT & BIT4){
+		retVal = TRUE;
+	}
+
+	return (retVal);              // return TRUE(pressed) or FALSE(not pressed)
 }
