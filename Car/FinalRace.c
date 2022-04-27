a55f131df3308b804680f3df1613a904ca03914d
ted5363 <47145977+ted5363@users.noreply.github.com>
2022-04-27 16:25:51 -0400

    Update FinalRace.c
    Cleaned up the code and removed extraneous comments.

--- Car/FinalRace.c
+++ Car/FinalRace.c
@@ -24,15 +24,11 @@ extern BOOLEAN g_sendData;
 static char str[100];
 #define CHAR_COUNT 10
 #define CENTER_SHIFT 7.2
-// lower = 4.7, upper = 9.7
 uint16_t smooth[128];
 int map[3];
 int setMode = 1;
 
 float el[3], er[3], VLact = 0, VRact = 0, pwml, pwmr, lastPosErr;
-//float centerErr[5];
-//float kp=0.4, ki=0.6, kd=0.1;
-//kp = 0.29 ki = 0.08 kd = 0.485
 float kp = 0.29, ki = 0.05, kd = 0.22;
 float baseSpeed;
 
@@ -76,14 +72,13 @@ void PID(float Vdes, float e[], float *Vact, float *pwm) {
 	*Vact = *pwm;
 }
 
-void forward(float speedL, float speedR, float shift, float posErr){
+void forward(float speed, float posErr){
   
 	float x = fabs(posErr); // fPosErr
 	float multiplier;
-	// Best run: 0.0065 - 0.038 + 0.1455 (0.75 max)
+	
+	// Calculate multiplier
 	if (x > 5) {
-	  //multiplier = (0.01275*x*x) + (-0.123*x) + 0.34425; // max 0.9
-		//multiplier = (0.0025*x*x) + (0.02*x) + 0.0675;
     multiplier = (0.00416667*x*x) + (0.00833333*x) + 0.1875;
 	} 
 	else if (x >= 13) {
@@ -93,35 +88,16 @@ void forward(float speedL, float speedR, float shift, float posErr){
 	  multiplier = 0;
 	}
 
+	// Calculate Speed error from positional error and multiplier
 	float speedErr = posErr * multiplier;
 	
-	PID(speedL - speedErr, el, &VLact, &pwml);
-	PID(speedR + speedErr, er, &VRact, &pwmr);
-  //float leftPID = pwml - speedErr;
-	//float rightPID = pwmr + speedErr;
+	// Calculate PID value for each motor
+	PID(speed - speedErr, el, &VLact, &pwml);
+	PID(speed + speedErr, er, &VRact, &pwmr);
 	
-	int max = 55, min = 0;
-	
-	/*if (leftPID > max) {
-		LED2_On(3);
-		leftPID = max;
-	} 
-	else if (leftPID < min) {
-		leftPID = min;
-	} 
-	else if (rightPID > max) {
-		LED2_On(3);
-    rightPID = max;
-	}		 
-	else if (rightPID < min) {
-		rightPID = min;
-	}*/
-	
+	// Send PWM value to motors
 	leftWheel(0.01*pwml);
 	rightWheel(0.01*pwmr);
-	
-	//sprintf(str,"Left Wheel: %.3f  Right Wheel: %.3f \n\r", leftPID, rightPID);
-	//uart2_put(str);
 	
 }
 
@@ -129,20 +105,23 @@ void backward(double speed){
 	TIMER_A2_PWM_DutyCycle(0.01*CENTER_SHIFT, 1);
 	leftWheel(-speed);
 	rightWheel(-speed);
-
 }
 
-void stop(void){/*Stop Car*/
-	forward(0.0, 0.0, 5.0, 64);/*backward(0.0) should produce same result*/
+void stop(void){
+	/*backward(0.0) should produce same result*/
+	backward(0.0);
 	//Disable Motor
 	P3->OUT &= ~BIT6;
 	P3->OUT &= ~BIT7;
-}
+} /*Stop Car*/
 
 void smoothData(void){
 	int max=0;
 	int j=1;
 	map[1] = 0;
+	
+	/* Take 5-point rolling average of Camera Data*/
+	
 	if (g_sendData == TRUE){
 		for (int i=0;i<128;i++){
 			switch(i) {
@@ -219,27 +198,8 @@ int main(void) {
 	LED2_On(2);
 	 
 	float center, shift, speed;
-	// baseSpeed = 27, kp = 0.375, ki = 0.35, kd = 0.45
-	// baseSpeed = 28, kp = 0.375, ki = 0.19, kd = 0.5
-	// baseSpeed = 30, kp = 0.375, ki = 0.24, kd = 0.5
-  //float kp = 0.375, ki = 0.24, kd = 0.5;	
-	
-	//sprintf(str,"p = %.2f, i = %.2f, d = %.2f\n\r", kp,ki,kd);
-	//uart2_put(str);
-	//forward(baseSpeed, baseSpeed, 5.0, 64);
-	
 	
 	for(;;){
-		/*
-		if (Switch1_Pressed() == TRUE){
-			LED2_On(6);
-			TIMER_A2_PWM_DutyCycle(0.05, 1);
-			myDelay(50);
-			forward(30, 30);
-			P3->OUT |= BIT6;
-			P3->OUT |= BIT7;
-		}
-		*/
 
 		smoothData();
 		
@@ -280,12 +240,6 @@ int main(void) {
       ki = 0.0;
 		}
 		
-		
-		//baseSpeed = (-(1.0/4.0))*((center-54.0)*(center-54.0)) + 40;
-		
-		//sprintf(str,"SPEED = %.3f\n\r", speed);
-	  //uart2_put(str);
-		
 		// Assign servo position
 		if (shift > CENTER_SHIFT-0.25 && shift < CENTER_SHIFT+0.25){
 		    shift = CENTER_SHIFT;  
@@ -297,17 +251,8 @@ int main(void) {
 		
 		float posErr = (64 - center);
 		
-		//speedErr = (kp * posErr) + (ki * (posErr + lastPosErr)/2) + (kd * (posErr - lastPosErr));
-		//lastPosErr = posErr;
-		
-		// Apply error calculations
-		// rightMotorSpeed = baseSpeed + speedErr;
-		// leftMotorSpeed = baseSpeed - speedErr;
-		
 		// Set motors to adjusted speeds
-		forward(speed, speed, shift, posErr);
-		//sprintf(str, "RM Speed = %.3f\n\rLM Speed = %.3F\n\r", rightMotorSpeed, leftMotorSpeed);
-		//uart2_put(str);
+		forward(speed, posErr);
 		
 	}
 	
